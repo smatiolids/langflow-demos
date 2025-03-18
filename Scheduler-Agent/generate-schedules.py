327cf1e4e3d8486fa36d9b63e2ac3fb962cb4d5b
Samuel Matioli <samuel.matioli@datastax.com>
2025-03-18 19:04:33 -0300

    date format

--- Scheduler-Agent/generate-schedules.py
+++ Scheduler-Agent/generate-schedules.py
@@ -18,24 +18,31 @@ schedule_collection = database.get_collection(
     "schedule_appointment",
 )
 
+
 def generate_schedules():
-    
-    specialties = ["cardiology", "dentist", "general_practice", "pediatrician", "psychiatrist", "surgeon"]
-    locations = ["New York", "Los Angeles", "Chicago", "Houston", "Miami", "San Francisco", "Seattle", "Washington D.C."]
-    providers = ["Dr. John Smith", "Dr. Jane Doe", "Dr. Jim Beam", "Dr. Jill Hill", "Dr. Jack Daniels", "Dr. John Doe", "Dr. Jane Smith"]
+
+    specialties = ["cardiology", "dentist", "general_practice",
+                   "pediatrician", "psychiatrist", "surgeon"]
+    locations = ["New York", "Los Angeles", "Chicago", "Houston",
+                 "Miami", "San Francisco", "Seattle", "Washington D.C."]
+    providers = ["Dr. John Smith", "Dr. Jane Doe", "Dr. Jim Beam",
+                 "Dr. Jill Hill", "Dr. Jack Daniels", "Dr. John Doe", "Dr. Jane Smith"]
     working_hours = {"start_time": "9:00", "end_time": "17:00"}
     appointment_duration = 30
     appointment_buffer = 15
-    dates = ["2025-03-19", "2025-03-20", "2025-03-21",  "2025-03-24", "2025-03-25", "2025-03-26", "2025-03-27", "2025-03-28"]
+    dates = ["2025-03-19", "2025-03-20", "2025-03-21",  "2025-03-24",
+             "2025-03-25", "2025-03-26", "2025-03-27", "2025-03-28"]
 
     for specialty in specialties:
         for location in locations:
             appointments = []
             for provider in providers:
-                start_time = datetime.strptime(working_hours["start_time"], "%H:%M")
-                end_time = datetime.strptime(working_hours["end_time"], "%H:%M")
+                start_time = datetime.strptime(
+                    working_hours["start_time"], "%H:%M")
+                end_time = datetime.strptime(
+                    working_hours["end_time"], "%H:%M")
                 current_time = start_time
-                
+
                 while current_time + timedelta(minutes=appointment_duration + appointment_buffer) < end_time:
                     for date in dates:
                         appointments.append({
@@ -43,31 +50,38 @@ def generate_schedules():
                             "location": location,
                             "provider": provider,
                             "date": date,
-                            "timestamp": datetime.strptime(f"{date}T{current_time.strftime('%H:%M')}:00.000+00:00", 
-                                                         "%Y-%m-%dT%H:%M:%S.%f%z"),
-                            "start_timestamp": current_time.strftime("%H:%M"),
-                            "end_timestamp": (current_time + timedelta(minutes=appointment_duration)).strftime("%H:%M")
+                            "timestamp": datetime.strptime(f"{date}T{current_time.strftime('%H:%M')}:00.000+00:00",
+                                                           "%Y-%m-%dT%H:%M:%S.%f%z"),
+                            "start_time": current_time.strftime("%H:%M"),
+                            "end_time": (current_time + timedelta(minutes=appointment_duration)).strftime("%H:%M")
                         })
-                        current_time += timedelta(minutes=appointment_duration + appointment_buffer)
-            
-            print(f"Inserting {len(appointments)} appointments for {specialty} in {location} with {provider}")
+                        current_time += timedelta(
+                            minutes=appointment_duration + appointment_buffer)
+
+            print(
+                f"Inserting {len(appointments)} appointments for {specialty} in {location} with {provider}")
             res = schedule_collection.insert_many(appointments)
             print(f"Inserted {len(res.inserted_ids)} appointments")
-            
+
 
 def delete_schedules():
     res = schedule_collection.delete_many({})
     print(f"Deleted {res.deleted_count} schedules")
 
+
 def get_schedules(condition):
     condition = condition.replace("'", '"')
     print("Condition:")
     print(condition)
-    res = schedule_collection.find(json.loads(condition))
+    # res = schedule_collection.find(json.loads(condition))
+    res = schedule_collection.find({"specialty": {"$eq": "surgeon"}, "timestamp": {
+                                   "$gt": datetime.strptime("2025-03-25T00:00:00Z", "%Y-%m-%dT%H:%M:%S%z"), "$lte": datetime.strptime("2025-03-26T00:00:00Z", "%Y-%m-%dT%H:%M:%S%z")}})
     print("Result:")
     print(res)
     for schedule in res:
+        print("--------------------------------")
         print(schedule)
+
 
 if __name__ == "__main__":
     if len(sys.argv) > 1 and sys.argv[1] == "generate":
